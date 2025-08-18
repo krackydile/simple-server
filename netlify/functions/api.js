4843b58aac8ee2cb468dad88d0547067716c0b5b
krackydile <locate.eric@gmail.com>
2025-08-17 19:32:00 -0700

    More

--- netlify/functions/api.js
+++ netlify/functions/api.js
@@ -1,9 +1,11 @@
-    exports.handler = async (event, context) => {
-        // Access query parameters
-        const name = event.queryStringParameters?.name || 'world';
+import express, { Router } from "express";
+import serverless from "serverless-http";
 
-        return {
-            statusCode: 200,
-            body: JSON.stringify({ message: `Hello, ${name}!` }),
-        };
-    };
+const api = express();
+
+const router = Router();
+router.get("/hello", (req, res) => res.send("Hello World!"));
+
+api.use("/api/", router);
+
+export const handler = serverless(api);
