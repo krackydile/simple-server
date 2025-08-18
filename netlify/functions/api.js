6d57a24cd1ecfa251e474c32698a0866e506e14a
krackydile <locate.eric@gmail.com>
2025-08-17 22:45:19 -0700

    Test

--- netlify/functions/api.js
+++
@@ -1,5 +0,0 @@
-// netlify/functions/api.js
-const serverless = require('serverless-http');
-const app = require('../../app'); // Adjust path based on your project structure
-
-exports.handler = serverless(app);
