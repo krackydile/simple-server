ad4bbf53bdcf1a53363499ad52622e1cb15ad8ff
krackydile <locate.eric@gmail.com>
2025-08-17 22:33:15 -0700

    Test

--- netlify/functions/api.js
+++ netlify/functions/api.js
@@ -1,11 +1,5 @@
-import express, { Router } from "express";
-import serverless from "serverless-http";
+// netlify/functions/api.js
+const serverless = require('serverless-http');
+const app = require('../../app'); // Adjust path based on your project structure
 
-const api = express();
-
-const router = Router();
-router.get("/hello", (req, res) => res.send("Hello World!"));
-
-api.use("/api/", router);
-
-export const handler = serverless(api);
+exports.handler = serverless(app);
